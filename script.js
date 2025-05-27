e70187d4130a7a35851be77d54ab769584db91ea
Shantanu <shantanudas0108@gmail.com>
2025-05-27 14:38:23 +0530

    added new index2 file

--- script.js
+++ script.js
@@ -1,71 +1,71 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-document.write(arr);
+// console.log(arr);
 
-//print array values to console.log using forEach method.
-console.log("printed arrays values using forEach method");
+// print array values to console.log using forEach method.
+// console.log("printed arrays values using forEach method");
 
-arr.forEach((item) => console.log(item));
+// arr.forEach((item) => console.log(item));
 
-//print even values to console.log using forEach method
-console.log("printed even values using forEach method");
+// //print even values to console.log using forEach method
+// console.log("printed even values using forEach method");
 
-let output = [];
+// let output = [];
 
-arr.forEach((item) => {
-  if (item % 2 === 0) {
-    output.push(item);
-  }
-});
+// arr.forEach((item) => {
+//   if (item % 2 === 0) {
+//     output.push(item);
+//   }
+// });
+// console.log(output);
+// output.forEach((item) => console.log(item));
 
-output.forEach((item) => console.log(item));
+// //print even values to console.log using filter method
+// console.log("printed even values using filter method");
 
-//print even values to console.log using filter method
-console.log("printed even values using filter method");
+// let filterEven = arr.filter((i) => i % 2 === 0);
 
-let filterEven = arr.filter((i) => i % 2 === 0);
+// console.log(filterEven);
 
-console.log(filterEven);
+// //print odd values to console.log using filter method
+// console.log("printed odd values using filter method");
 
-//print odd values to console.log using filter method
-console.log("printed odd values using filter method");
+// let filterOdd = arr.filter((i) => i % 2 !== 0);
 
-let filterOdd = arr.filter((i) => i % 2 !== 0);
+// console.log(filterOdd);
 
-console.log(filterOdd);
+// //check arr have number 5 or not by using find method
+// console.log("checked arr have number 5 or not by using find method");
 
-//check arr have number 5 or not by using find method
-console.log("checked arr have number 5 or not by using find method");
+// let found = arr.find((i) => i == 5);
+// console.log(found);
 
-let found = arr.find((i) => i == 5);
-console.log(found);
+// //check arr have number 11 or not by using find method
+// console.log("checked arr have number 11 or not by using find method");
 
-//check arr have number 11 or not by using find method
-console.log("checked arr have number 11 or not by using find method");
+// let notFound = arr.find((i) => i == 11);
+// console.log(notFound);
 
-let notFound = arr.find((i) => i == 11);
-console.log(notFound);
+// //check the position of number 5 by using findIndex method
+// console.log("checked the position of number 5 by using findIndex method");
 
-//check the position of number 5 by using findIndex method
-console.log("checked the position of number 5 by using findIndex method");
+// let findIx = arr.findIndex((i) => i == 1);
+// console.log(findIx);
 
-let findIx = arr.findIndex((i) => i == 5);
-console.log(findIx);
+// //check the position of number 11 by using findIndex method
+// console.log("checked the position of number 11 by using findIndex method");
 
-//check the position of number 11 by using findIndex method
-console.log("checked the position of number 11 by using findIndex method");
+// let find_11 = arr.findIndex((i) => i == 11);
+// console.log(find_11);
 
-let find_11 = arr.findIndex((i) => i == 11);
-console.log(find_11);
+// //duble the array's values by using map method
+// console.log("dubled the array's values by using map method");
 
-//duble the array's values by using map method
-console.log("dubled the array's values by using map method");
-
-dblArray = arr.map((i) => i * 2);
+dblArray = arr.filter((i) => i * 2);
 console.log(dblArray);
 
-//half the array's values by using map method
-console.log("halfed the array's values by using map method");
+// //half the array's values by using map method
+// console.log("halfed the array's values by using map method");
 
-halfArray = arr.map((i) => i / 2);
-console.log(halfArray);
+// halfArray = arr.map((i) => i / 2);
+// console.log(halfArray);
